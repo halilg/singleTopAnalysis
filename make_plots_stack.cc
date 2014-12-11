aa18211b49bf4247069b11e3fc913bd7a674eeca
Halil Gamsızkan <halilgamsizkan@gmail.com>
2014-12-11 17:56:21 +0200

    stack histograms available

--- make_plots_stack.cc
+++ make_plots_stack.cc
@@ -1,54 +1,77 @@
-void make_plots_shapecomparison(TString dpath, TString hn, TString extension){
+void make_plots_stack(TString dpath, TString hn, TString extension){
 	
 	TFile tfdata("h-TreesEle_Data_2J_0T_noSyst.root");
-	TFile tftch("TreesEle_TChannel_2J_0T_noSyst.root");
-	TFile tftwch("TreesEle_TWChannel_2J_0T_noSyst.root");
+	TFile tftch("h-TreesEle_TChannel_2J_0T_noSyst.root");
+	TFile tftwch("h-TreesEle_TWChannel_2J_0T_noSyst.root");
 	//TFile tfsch();
-	TFile tfqcd("TreesEle_QCDEle_2J_0T_noSyst.root");
-	TFile tfwj("TreesEle_WJets_2J_0T_noSyst.root");
-	TFile tfzj("TreesEle_ZJets_2J_0T_noSyst.root");
-	TFile tftt("h-TreesEle_TTBar_2J_1T_noSyst.root");
+	TFile tfqcd("h-TreesEle_QCDEle_2J_0T_noSyst.root");
+	TFile tfwj("h-TreesEle_WJets_2J_0T_noSyst.root");
+	TFile tfzj("h-TreesEle_ZJets_2J_0T_noSyst.root");
+	TFile tftt("h-TreesEle_TTBar_2J_0T_noSyst.root");
 	//TFile tfdb();
-
-//TreesEle_ZZ_2J_0T_noSyst.root		TreesEle_WW_2J_0T_noSyst.root		TreesEle_WZ_2J_0T_noSyst.root
-	
-			
+	//TreesEle_ZZ_2J_0T_noSyst.root		TreesEle_WW_2J_0T_noSyst.root		TreesEle_WZ_2J_0T_noSyst.root
 	
 	
-	return;
-	
-	if (! tf0.IsOpen() ){
-		cout << "Couldn't open file: " << rfn0 << endl;
-		return;
-	}
-	if (! tf1.IsOpen() ){
-		cout << "Couldn't open file: " << rfn1 << endl;
-		return;
-	}
 	cout << "accessing histograms\n";
-	TH1D* h0 = (TH1D*) tf0.Get(hn);
-	TH1D* h1 = (TH1D*) tf1.Get(hn);
+	TH1D* hdata = (TH1D*) tfdata.Get(hn);
+	TH1D* htch = (TH1D*) tftch.Get(hn); 
+	TH1D* htwch = (TH1D*) tftwch.Get(hn); 
+	TH1D* hqcd = (TH1D*) tfqcd.Get(hn); 
+	TH1D* hwj = (TH1D*) tfwj.Get(hn); 
+	TH1D* hzj = (TH1D*) tfzj.Get(hn); 
+	TH1D* htt = (TH1D*) tftt.Get(hn); 
 
-	// normalize histograms
-	Double_t norm = h0->GetEntries();
-	h0->Scale(1/norm);
-	norm = h1->GetEntries();
-	h1->Scale(1/norm);
+	//hdata->SetFillColor(kRed);
+	htch->SetFillColor(kGreen);htch->SetLineColor(kGreen);;
+	htwch->SetFillColor(kBlue);htwch->SetLineColor(kBlue);
+	hqcd->SetFillColor(kOrange);hqcd->SetLineColor(kOrange);
+	hwj->SetFillColor(kRed);hwj->SetLineColor(kRed);
+	hzj->SetFillColor(kTeal);hzj->SetLineColor(kTeal);
+	htt->SetFillColor(kAzure);htt->SetLineColor(kAzure);
+	
+	hdata->SetMarkerStyle(20);
+	//htch->SetMarkerStyle(21);
+	//htwch->SetMarkerStyle(21);
+	//hqcd->SetMarkerStyle(21);
+	//hwj->SetMarkerStyle(21);
+	//hzj->SetMarkerStyle(21);
+	//htt->SetMarkerStyle(21);
+
+	//hdata->SetMarkerColor(kRed);	
+	//htch->SetMarkerColor(kRed);	
+	//htwch->SetMarkerColor(kRed);	
+	//hqcd->SetMarkerColor(kRed);	
+	//hwj->SetMarkerColor(kRed);	
+	//hzj->SetMarkerColor(kRed);	
+	//htt->SetMarkerColor(kRed);	
+	
+	THStack hs("hs","Stacked 1D histograms");
+	
+	hs.Add(htch);
+	hs.Add(hwj);
+	hs.Add(htt);
+	hs.Add(hzj);
+	hs.Add(hqcd);
+	hs.Add(htwch);
+	
 	
 	TCanvas c;
-	TLegend leg(.7, .8, .9, .9);
-	leg.AddEntry(h0, "T Channel", "l");
-	leg.AddEntry(h1, "TTBar", "l");
-	h1->SetLineColor(8);
-	h1->SetLineStyle(2);
+	TLegend leg(.7, .6, .9, .9);
+	leg.AddEntry(hdata, "Data", "LP");
+	leg.AddEntry(htch, "T-Channel", "l");
+	leg.AddEntry(hwj, "W+Jets", "l");
+	leg.AddEntry(htt, "TTBar", "l");
+	leg.AddEntry(hzj, "Z+Jets", "l");
+	leg.AddEntry(hqcd, "QCD", "l");
+	leg.AddEntry(htwch, "TW-Channel", "l");
 	
 	gStyle->SetOptStat(0);
 	gStyle->SetOptTitle(0);
-	h1->Draw();
-	h0->Draw("SAME");
-	leg.Draw();
+	hdata->Draw("E1");
+	hs.Draw("SAME");
+	leg.Draw("SAME");
+	//leg.Draw();
 	TString pname = dpath+hn+extension;
 	//cout << "wrot"pname << endl;
-	
 	c.Print(pname);
 }
--- make_plots_stack.zsh
+++ make_plots_stack.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-dpath=\"plots/combined/\"
+dpath=\"plots/stack/\"
 rfn0=\"h-TreesEle_TChannel_2J_1T_noSyst.root\"
 rfn1=\"h-TreesEle_TTBar_2J_1T_noSyst.root\"
-hn=\"h_fJetEta\"
+hn=\"h_mtwMass\"
 extension=\".pdf\"
 
 root.exe -l -b -q "make_plots_stack.cc($dpath, $hn, $extension)"
