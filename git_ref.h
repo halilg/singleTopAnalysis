d0d3b7cc379a380e108344d084a8733917ac6cd0
Halil Gamsizkan <halilgamsizkan@gmail.com>
2014-12-11 21:25:01 +0200

    small fix

--- git_ref.h
+++ git_ref.h
@@ -1,1 +1,1 @@
-#define GIT_REF "8c3e8d8cb4"
+#define GIT_REF "d7a674eeca"
--- make_plots_stack.zsh
+++ make_plots_stack.zsh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 
-dpath=\"plots/stack/\"
+dpath=plots/stack
+hn=h_mtwMass
+mkdir -p $dpath
+
+dpath=\"$dpath\"
+
 rfn0=\"h-TreesEle_TChannel_2J_1T_noSyst.root\"
 rfn1=\"h-TreesEle_TTBar_2J_1T_noSyst.root\"
-hn=\"h_mtwMass\"
+hn=\"$hn\"
 extension=\".pdf\"
 
 root.exe -l -b -q "make_plots_stack.cc($dpath, $hn, $extension)"
