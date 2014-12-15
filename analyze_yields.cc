53ad05b856dab8074168d38e9adbd29ce031ccb3
Halil Gamsızkan <halilgamsizkan@gmail.com>
2014-12-15 13:13:44 +0200

    added files

---
+++ analyze_yields.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <json/json.h>
+#include <cfgreader.h>
+
+using namespace std;
+int main(){
+    cfgreader cfg("analysis.cfg");
+    cout << cfg.json_path << endl;
+    for (int i =0; i<cfg.datasets.size(); i++){
+        cout << cfg.datasets[i] << endl;
+    }
+}
