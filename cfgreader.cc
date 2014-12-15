e3af82971278daa0edb41822c84f4e68c9f47ef6
Halil Gamsızkan <halilgamsizkan@gmail.com>
2014-12-15 13:04:15 +0200

    added files

---
+++ cfgreader.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <json/json.h>
+
+using namespace std;
+class cfgreader {
+  public:
+    cfgreader (string);
+    std::vector<string> datasets;
+    std::vector<string> categories;
+    string jsonpath;
+};
+
+cfgreader::cfgreader (string cfgfile){
+    Json::Value root;   // will contains the root value after parsing
+    Json::Reader reader;
+    std::string line, inputConfig;
+    
+    // read global configuration
+    cout << "Reading global configuration " << cfgfile << " : ";
+    std::ifstream myfile (cfgfile);
+    if (myfile.is_open()){
+        while ( getline (myfile,line) ){ inputConfig += line; }
+        myfile.close();
+    } else {
+      std::cerr << "Unable to open file test.json";
+      exit(1);
+    }
+    
+    cout << "parsing json : ";
+    bool parsingSuccessful = reader.parse( inputConfig, root );
+    if ( !parsingSuccessful )
+    {
+        // report to the user the failure and their locations in the document.
+        std::cerr  << "Failed to parse configuration\n"
+                   << reader.getFormattedErrorMessages();
+        exit(1);
+    }
+    cout << "success.\n";
+    
+    // read the configuration
+    //vector<string> datasets;
+    //vector<string> categories;
+    string json_path(root["directories"]["json"].asString());
+    for (int i = 0; i<root["datasets"].size(); i++) {
+        datasets.push_back(root["datasets"][i].asString());
+    }
+    for (int i = 0; i<root["categories"].size(); i++) {
+        categories.push_back(root["categories"][i].asString());
+    }
+    std::cout << "number of Datasets: " << datasets.size() << endl;
+    std::cout << "number of categories: " << categories.size() << endl;
+    std::cout << "json path: " << json_path << endl;    
+}
---
+++ cfgreader.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+class cfgreader {
+  public:
+    cfgreader (std::string);
+    std::vector<std::string> datasets;
+    std::vector<std::string> categories;
+    std::string jsonpath;
+};
